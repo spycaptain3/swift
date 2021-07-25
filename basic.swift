2225534804f08a9d69a82756e69e11c52528f679
Deepak Gupta <spycaptain3@gmail.com>
2021-07-25 21:00:06 +0530

    Basic Swift code

---
+++ basic.swift
@@ -0,0 +1,14 @@
+print("Hello swift")
+let name:String="Deepak Gupta"
+print(name,separator:"is",terminator:" my name")
+let cool:String="and cool"
+for y in 1...23
+{
+    print("Swift is swift",(cool))
+}
+print(UInt.max)
+print(Int8.max)
+print(Int64.min)
+var dogname="Brudo"
+print("\"",dogname,"\" is the best")
+print(dogname)
